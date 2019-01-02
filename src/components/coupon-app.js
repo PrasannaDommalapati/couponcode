d223b491f411614f933d706f76dfd91e775aa2c5
Prasanna KUMAR <prasanna.kumar@axa-ppp.co.uk>
2019-01-02 12:00:47 +0000

    react and jest working

--- src/components/coupon-app.js
+++ src/components/coupon-app.js
@@ -2,19 +2,15 @@ import React from 'react';
 // import logo from './logo.svg';
  import './coupon-app.scss';
 
-class CouponApp extends React.Component {
- 
+ export default class CouponApp extends React.Component {
+   
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1>Welcome to Coupon code Appppp</h1>
+          <h1>Welcome to Coupon App</h1>
         </header>
       </div>
     );
   }
 }
-
-export default CouponApp;
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,52 @@
 const path = require('path');
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const sassRegex = /\.(scss|sass|css)$/;
+const jsxRegex = /\.(js|jsx)$/;
 require('dotenv').config();
 
-const config = require('./bin/config');
 
-module.exports = {
-  context: path.resolve(__dirname, './src'),
-  entry: {
-    app: './app.js'
-  },
+module.exports = (env, argv) => ({
+  
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].bundle.js'
   },
-  devServer: config.devServer,
-  module: config.module,
-  plugins: config.plugins
-};
+  watch: true,
+  devtool: argv.mode == 'development' ? 'source-map' : false,
+  performance: { hints: false },
+  devServer: {
+    historyApiFallback: true,
+    host: process.env.DEV_SERVER_HOST || 'localhost',
+    port: process.env.DEV_SERVER_PORT || 5000
+  },
+  module: {
+    rules: [
+      {
+        test: jsxRegex,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ],
+      },
+      {
+        test: sassRegex,
+        loaders: [
+          require.resolve('style-loader'),
+          require.resolve('css-loader'),
+          require.resolve('sass-loader'),
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ]
+});
+
