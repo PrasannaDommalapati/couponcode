0abaaf32c6ce8f7a5cb5ef826bdf52c29291edb7
Prasanna KUMAR <prasanna.kumar@axa-ppp.co.uk>
2019-01-03 15:09:41 +0000

    reactstrap

---
+++ src/components/header/component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class AppHeader extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className= "appHeader">Promotion Triumph</div>
+        );
+    }
+}
