bd518d72f4e2e978c51af9220c04a0c7c743d5ac
Prasanna Kumar <prasanna.kumar@headforwards.com>
2019-01-04 23:40:45 +0000

    favicon test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CouponApp from './components/coupon-app';
-import './index.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {Route, Link, BrowserRouter as Router} from 'react-router-dom';
 import AppHeader from "./components/header/component";
 import FooterComponent from "./components/footer/component";
 import LoginComponent from "./components/profile/login/component";
 import RegisterComponent from "./components/profile/register/component";
+import './index.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../favicon.png';
 
 const routing = (
     <Router>
