65c788cbb3fec7f3b1fee5f01650746ea8e46f09
Prasanna KUMAR <prasanna.kumar@axa-ppp.co.uk>
2019-01-04 15:26:25 +0000

    login and react forms finished

--- src/components/profile/login/component.js
+++ src/components/profile/login/component.js
@@ -1,19 +1,22 @@
-import React, {Component} from "react";
-import {Button, Form, FormGroup, Label, Input, FormText} from 'reactstrap';
+import React, { Component } from "react";
+import {Link} from "react-router-dom";
+import {Button, Form, FormGroup, Input} from 'reactstrap';
+import './component.scss';
 
 export default class LoginComponent extends Component {
 
     render() {
         return (
-            <Form name="login">
+            <Form name="login" >
+                <h1>login</h1>
                 <FormGroup>
-                    <Label for="email">Email</Label>
                     <Input type="email" name="loginEmail" id="email" placeholder="please enter email here.."/>
                 </FormGroup>
                 <FormGroup>
-                    <Label for="password">Password</Label>
                     <Input type="password" name="loginPassword" id="password" placeholder="please enter password here.."/>
                 </FormGroup>
+                <Link to="/forgot">Forgot Password</Link>
+                <Button>Login</Button>
             </Form>
         );
     }
--- src/components/profile/register/component.js
+++ src/components/profile/register/component.js
@@ -1,19 +1,26 @@
 import React, {Component} from "react";
-import {Button, Form, FormGroup, Label, Input, FormText} from 'reactstrap';
+import {Button, Form, FormGroup, Input, FormText} from 'reactstrap';
+import './component.scss';
 
 export default class RegisterComponent extends Component {
     
     render() {
         return (
             <Form name="register">
+                <h1>register</h1>
                 <FormGroup>
-                    <Label for="registerEmail">Email</Label>
                     <Input type="email" name="registerEmail" id="registerEmail" placeholder="please enter email here.."/>
                 </FormGroup>
                 <FormGroup>
-                    <Label for="registerPassword">Password</Label>
                     <Input type="password" name="registerPassword" id="registerPassword" placeholder="please enter password here.."/>
                 </FormGroup>
+                <FormGroup>
+                    <Input type="text" name="firstName" id="firstname" placeholder="First name here"/>
+                </FormGroup>
+                <FormGroup>
+                    <Input type="textarea" name="about" id="about"/>
+                </FormGroup>
+                <Button>Register</Button>
             </Form>
         );
     }
