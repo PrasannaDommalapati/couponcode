925ff430c9ecb387a7e2ccd1e8a83bf24bd4fcd9
Prasanna KUMAR <prasanna.kumar@axa-ppp.co.uk>
2019-01-07 17:24:19 +0000

    Login component views tested

--- src/__tests__/login.spec.js
+++ src/__tests__/login.spec.js
@@ -3,8 +3,20 @@ import {shallow} from '../setupTests';
 import LoginComponent from "../components/profile/login/component";
 
 describe('Login Component', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<LoginComponent/>);
+    });
+
     it('should render with an email field', () => {
-        const wrapper = shallow(<LoginComponent/>);
-        // expect(wrapper.find('.form-control').children()).toBe(true)
+        expect(wrapper.find('#email').length).toEqual(1);
+    });
+
+    it('should render with a password field', () => {
+        expect(wrapper.find('#password').length).toEqual(1);
+    });
+
+    it('should render with an actions section',() => {
+        expect(wrapper.find('.actions').length).toEqual(1)
     });
 });
