e1e158f9e61873421cd9d90870ad96ab9976db3a
PranithaSree Malepatichendu <pranithasree.malepatichendu@headforwards.com>
2019-04-10 22:11:27 +0100

    removed babel rc

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    presets: [
+      [
+        "@babel/env",
+        "@babel/react"
+      ]
+    ],
+    env: {
+      test: {
+        plugins: ['@babel/plugin-proposal-class-properties']
+      }
+    }
+  }
